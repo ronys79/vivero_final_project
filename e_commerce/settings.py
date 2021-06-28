78243c56f803d2372d476cdfbd87f98d05957009
ronys79 <ronyserrato@gmail.com>
2021-06-28 17:11:44 +0300

    added package config

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['vivero-course-env.eba-tymm8mrp.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = [
+    'vivero-course-env.eba-tymm8mrp.us-west-2.elasticbeanstalk.com',
+    'localhost',
+    ]
 
 
 # Application definition
